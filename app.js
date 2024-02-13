c2d4b32d0391a85c98a249505f091e135388d841
Ryz Clyd Sabucido <rcsabucido@gmail.com>
2024-02-13 12:11:46 +0800

    Add basic hover function to grid

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const gridContainer = document.querySelector(".gridContainer"); 
 
 for (let i = 0; i < 256; i++) {
-   let gridDiv = document.createElement("div");
+    let gridDiv = document.createElement("div");
     gridDiv.className = "grid"
 
     gridContainer.appendChild(gridDiv);
+    gridDiv.classList.add("colorGrid");
 }
+
+const rgbGrid = document.querySelectorAll(".colorGrid");
+
+rgbGrid.forEach((grid) => {
+    grid.addEventListener("mouseover", () => {
+        grid.style.backgroundColor = "red";
+    })
+})
+
+
+// divContainer / given grid = dimension of the grid
+// 300 x 300px container / 3x3 grid = 100px 
