b6f134d6a4ffa4a0faf87c6726f092d9f7d207aa
Ryz Clyd Sabucido <rcsabucido@gmail.com>
2024-11-26 15:04:50 +0800

    Add functions to make the program create a new canvas and change dimensions

--- app.js
+++ app.js
@@ -8,31 +8,45 @@ function createCanvas(dim) {
         gridDiv.className = "grid"
     
         gridContainer.appendChild(gridDiv);
-        gridDiv.classList.add("colorGrid");
     }
 }
 
 document.addEventListener("load", createCanvas(256));
 
-let rgbGrid = document.querySelectorAll(".colorGrid");
+function createMouseListener() {
+    let rgbGrid = document.querySelectorAll(".grid");
 
-rgbGrid.forEach((grid) => {
-    grid.addEventListener("mouseover", () => {
-        grid.style.backgroundColor = "red";
+    rgbGrid.forEach((grid) => {
+        grid.addEventListener("mouseover", () => {
+            grid.style.backgroundColor = "red";
+        })
     })
-})
+}
 
+function changeGridSize(dim) {
+    let rgbGrid = document.querySelectorAll(".grid");
+    let size = String((640 / dim));
+    rgbGrid.forEach((grid) => {
+        grid.style.width = size.concat("px");
+        grid.style.height = size.concat("px");
+    });
+}
 
 addBtn.addEventListener("click", () => {
     let userInput = prompt("Please enter the dimension:");
     let newDimension = userInput * userInput;
 
+    let rgbGrid = document.querySelectorAll(".grid");
     rgbGrid.forEach((grid) => {
         grid.remove();
     })
 
     createCanvas(newDimension);
+    changeGridSize(userInput);
+    createMouseListener();
 });
+
+createMouseListener();
 
 // divContainer / given grid = dimension of the grid
 // 300 x 300px container / 3x3 grid = 100px 
